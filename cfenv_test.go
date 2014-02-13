95627336c3820c4301cd61e0ae636dd129a96f5a
Joe Fitzgerald <jfitzgerald@gopivotal.com>
2014-02-12 23:36:18 -0600

    Translate environment variables into map[string]string

---
+++ cfenv_test.go
@@ -0,0 +1,11 @@
+package cfenv_test
+
+import (
+	//. "github.com/joefitzgerald/cfenv"
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Cfenv", func() {
+
+})
---
+++ envmap.go
@@ -0,0 +1,29 @@
+package cfenv
+
+import (
+	"os"
+	"strings"
+)
+
+func Variables() map[string]string {
+	vars := mapEnv(os.Environ(), splitEnv())
+	return vars
+}
+
+func splitEnv() func(item string) (key, val string) {
+	return func(item string) (key, val string) {
+		splits := strings.Split(item, "=")
+		key = splits[0]
+		val = strings.Join(splits[1:], "=")
+		return
+	}
+}
+
+func mapEnv(data []string, keyFunc func(item string) (key, val string)) map[string]string {
+	items := make(map[string]string)
+	for _, item := range data {
+		key, val := keyFunc(item)
+		items[key] = val
+	}
+	return items
+}

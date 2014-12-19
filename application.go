d89ed09b011ab14b08ac0616288e07d743c7a2bf
Joe Fitzgerald <jfitzgerald@pivotal.io>
2014-12-19 10:01:52 -0700

    :lipstick: ApplicationURIs Are More Than A Single URI

--- application.go
+++ application.go
@@ -2,17 +2,17 @@ package cfenv
 
 // An App holds information about the current app running on Cloud Foundry
 type App struct {
-	ID             string   `json:"instance_id"`      // id of the app
-	Index          int      `json:"instance_index"`   // index of the app
-	Name           string   `json:"name"`             // name of the app
-	Host           string   `json:"host"`             // host of the app
-	Port           int      `json:"port"`             // port of the app
-	Version        string   `json:"version"`          // version of the app
-	ApplicationURI []string `json:"application_uris"` // application uri of the app
-	Home           string   // root folder for the deployed app
-	MemoryLimit    string   // maximum amount of memory that each instance of the application can consume
-	WorkingDir     string   // present working directory, where the buildpack that processed the application ran
-	TempDir        string   // directory location where temporary and staging files are stored
-	User           string   // user account under which the DEA runs
-	Services       Services // services bound to the app
+	ID              string   `json:"instance_id"`      // id of the app
+	Index           int      `json:"instance_index"`   // index of the app
+	Name            string   `json:"name"`             // name of the app
+	Host            string   `json:"host"`             // host of the app
+	Port            int      `json:"port"`             // port of the app
+	Version         string   `json:"version"`          // version of the app
+	ApplicationURIs []string `json:"application_uris"` // application uri of the app
+	Home            string   // root folder for the deployed app
+	MemoryLimit     string   // maximum amount of memory that each instance of the application can consume
+	WorkingDir      string   // present working directory, where the buildpack that processed the application ran
+	TempDir         string   // directory location where temporary and staging files are stored
+	User            string   // user account under which the DEA runs
+	Services        Services // services bound to the app
 }

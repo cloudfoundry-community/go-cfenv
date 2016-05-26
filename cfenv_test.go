42d28a02b71321d28b8130f3b91211c05544ef01
Josh Kruck <joshua.kruck@gmail.com>
2016-05-26 19:34:20 -0400

    Add test to make sure that legacy uses (pre space_id and space_name) don't blow up

--- cfenv_test.go
+++ cfenv_test.go
@@ -19,6 +19,16 @@ var _ = Describe("Cfenv", func() {
 			`VCAP_SERVICES={"elephantsql-dev":[{"name":"elephantsql-dev-c6c60","label":"elephantsql-dev","tags":["New Product","relational","Data Store","postgresql"],"plan":"turtle","credentials":{"uri":"postgres://seilbmbd:PHxTPJSbkcDakfK4cYwXHiIX9Q8p5Bxn@babar.elephantsql.com:5432/seilbmbd"}}],"sendgrid":[{"name":"mysendgrid","label":"sendgrid","tags":["smtp","Email"],"plan":"free","credentials":{"hostname":"smtp.sendgrid.net","username":"QvsXMbJ3rK","password":"HCHMOYluTv"}}]}`,
 		}
 
+		validEnvWithoutSpaceIDAndName := []string{
+			`VCAP_APPLICATION={"instance_id":"451f045fd16427bb99c895a2649b7b2a","instance_index":0,"host":"0.0.0.0","port":61857,"started_at":"2013-08-12 00:05:29 +0000","started_at_timestamp":1376265929,"start":"2013-08-12 00:05:29 +0000","state_timestamp":1376265929,"limits":{"mem":512,"disk":1024,"fds":16384},"application_version":"c1063c1c-40b9-434e-a797-db240b587d32","application_name":"styx-james","application_uris":["styx-james.a1-app.cf-app.com"],"version":"c1063c1c-40b9-434e-a797-db240b587d32","name":"styx-james","uris":["styx-james.a1-app.cf-app.com"],"users":null}`,
+			`HOME=/home/vcap/app`,
+			`MEMORY_LIMIT=512m`,
+			`PWD=/home/vcap`,
+			`TMPDIR=/home/vcap/tmp`,
+			`USER=vcap`,
+			`VCAP_SERVICES={"elephantsql-dev":[{"name":"elephantsql-dev-c6c60","label":"elephantsql-dev","tags":["New Product","relational","Data Store","postgresql"],"plan":"turtle","credentials":{"uri":"postgres://seilbmbd:PHxTPJSbkcDakfK4cYwXHiIX9Q8p5Bxn@babar.elephantsql.com:5432/seilbmbd"}}],"sendgrid":[{"name":"mysendgrid","label":"sendgrid","tags":["smtp","Email"],"plan":"free","credentials":{"hostname":"smtp.sendgrid.net","username":"QvsXMbJ3rK","password":"HCHMOYluTv"}}]}`,
+		}
+
 		envWithIntCredentials := []string{
 			`VCAP_APPLICATION={"instance_id":"451f045fd16427bb99c895a2649b7b2a","instance_index":0,"host":"0.0.0.0","port":61857,"started_at":"2013-08-12 00:05:29 +0000","started_at_timestamp":1376265929,"start":"2013-08-12 00:05:29 +0000","state_timestamp":1376265929,"limits":{"mem":512,"disk":1024,"fds":16384},"application_version":"c1063c1c-40b9-434e-a797-db240b587d32","application_name":"styx-james","application_uris":["styx-james.a1-app.cf-app.com"],"version":"c1063c1c-40b9-434e-a797-db240b587d32","name":"styx-james","uris":["styx-james.a1-app.cf-app.com"],"users":null}`,
 			`HOME=/home/vcap/app`,
@@ -117,6 +127,17 @@ var _ = Describe("Cfenv", func() {
 				}
 				Ω(isValidTags).Should(BeTrue(), "Not valid tags when finding by regex")
 
+			})
+		})
+
+		Context("Without a space name and id", func() {
+			It("Should deserialize correctly", func() {
+				testEnv := Env(validEnvWithoutSpaceIDAndName)
+				cfenv, err := New(testEnv)
+				Ω(err).Should(BeNil())
+				Ω(cfenv).ShouldNot(BeNil())
+				Ω(cfenv.SpaceID).Should(BeEmpty())
+				Ω(cfenv.SpaceName).Should(BeEmpty())
 			})
 		})
 

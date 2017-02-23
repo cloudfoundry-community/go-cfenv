e9e2bf2fe090a027eb409255fa375171851925f4
Ben Hartshorne <ben@hound.sh>
2017-02-23 13:39:30 -0600

    adding application ID to go-cfenv, changing the name of ID to InstanceID to make it clear which ID it means.

--- application.go
+++ application.go
@@ -2,7 +2,9 @@ package cfenv
 
 // An App holds information about the current app running on Cloud Foundry
 type App struct {
-	ID              string   `json:"instance_id"`      // id of the app
+	ID              string   `json:"-"`                // DEPRECATED id of the instance
+	InstanceID      string   `json:"instance_id"`      // id of the instance
+	AppID           string   `json:"application_id"`   // id of the application
 	Index           int      `json:"instance_index"`   // index of the app
 	Name            string   `json:"name"`             // name of the app
 	Host            string   `json:"host"`             // host of the app
@@ -17,4 +19,12 @@ type App struct {
 	TempDir         string   // directory location where temporary and staging files are stored
 	User            string   // user account under which the DEA runs
 	Services        Services // services bound to the app
+	CFAPI           string   `json:"cf_api"` // URL for the Cloud Foundry API endpoint
+	Limits          *Limits  `json:"limits"` // limits imposed on this process
+}
+
+type Limits struct {
+	Disk int `json:"disk"` // disk limit
+	FDs  int `json:"fds"`  // file descriptors limit
+	Mem  int `json:"mem"`  // memory limit
 }
